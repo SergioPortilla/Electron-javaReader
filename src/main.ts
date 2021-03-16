391dec86f15f443733e86dca37e22f9d088563c2
Sergio Portilla <sergio.portilla@scotiabank.com.co>
2021-03-16 00:41:32 -0500

    End first activity

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ export default class Main {
     }
 
     private static onReady() {
-        Main.mainWindow = new Main.BWindow({ width: 1200, height: 800, webPreferences: { nodeIntegration: true }, darkTheme: true });
+        Main.mainWindow = new Main.BWindow({ width: 1800, height: 1200, webPreferences: { nodeIntegration: true }, darkTheme: true });
         Main.mainWindow.loadFile('./index.html').then(r => console.log(r));
         Main.mainWindow.on('closed', Main.onWindowAllClosed);
     }
@@ -25,13 +25,14 @@ export default class Main {
     }
 }
 
+
 /** ************************************************************************************* */
 export function algo(input: any) {
-    let inFile: HTMLElement | null  = document.getElementById("inputfile");
+    let inFile: HTMLElement | null  = document.getElementById("inputFile");
     let outText: HTMLElement | null = document.getElementById("output");
-    if (inFile && outText) {
-    //     inFile.style.display = 'none';
+    if (outText && inFile) {
         outText.style.display = 'block';
+        isOpenFile();
     }
 
     const RGX = {
@@ -41,8 +42,9 @@ export function algo(input: any) {
         enum: /(enum |(?:public|private|protected)+ enum )(\S*)/,
         interface: /(interface |(?:public|private|protected)+ interface )(\S*)/,
         interface2: /((?:implements ))(\S*)/,
-        reserved: /(if|else|for|try|cath|return|new)/,
-        method: /((?:public|private|protected) \w*)(\S*)(;)/
+        reserved: /(if|else|for|try|cath|return|new)(\s)/,
+        attribute: /((?:public|private|protected) \S* )(\S*;)/,
+        method: /((?:public|private|protected) \S* )(\S*\()/
     };
     let file = input.files[0];
     let reader = new FileReader();
@@ -51,18 +53,21 @@ export function algo(input: any) {
     reader.onload = function() {
         if (alguito && typeof reader.result === "string") {
             let array = reader.result.split(RGX.enter);
+            // let line = reader.result;
             let result: string = "";
             let number = 1;
             array.forEach(line => {
-                line = regexResolver(line, RGX.method, 'rgb(148,69,8)','method');
+                line = regexResolver(line, RGX.attribute, 'rgb(123,207,199)','attribute');
+                line = regexResolver(line, RGX.method, 'rgb(73,212,15)','method');
                 line = regexResolver(line, RGX.import, 'green', 'import');
                 line = regexResolver(line, RGX.class, 'rgb(94,214,144)','class');
                 line = regexResolver(line, RGX.enum, 'rgb(15,160,160)','enum');
                 line = regexResolver(line, RGX.interface, 'rgb(143,148,8)','interface');
                 line = regexResolver(line, RGX.interface2, 'rgb(143,148,8)','interface');
-                // line = regexResolver(line, RGX.reserved, 'rgb(109,110,233)');
+                line = regexResolver(line, RGX.reserved, 'rgb(109,110,233)');
                 result = result + '\n' + '<a class="number">'+number++ + '</a>' + '<a class="line">'+line+'</a>';
             });
+            // alguito.innerHTML = line;
             alguito.innerHTML = result;
         }
     };
@@ -72,18 +77,41 @@ export function algo(input: any) {
     };
 }
 
-const eta = false;
-
 function regexResolver(line: string, regex: RegExp, color: string, element: string = ''): string {
     let data = element === '' ? undefined : document.getElementById(element);
     if (regex.test(line) && line.match(regex)) {
-        line = line.replace(regex ,`<div style="color: ${color}">$1</div>$2`);
+        line = line.replace(regex ,`<p style="color: ${color}">$1</p>$2`);
 
         if (data) {
             // @ts-ignore
-            data.innerHTML += '\n' + line.match(regex)[2];
+            data.innerHTML += line.match(regex)[2].replace(/(?:{|;|\()/gm, '') + '\n';
             data.style.display = 'block';
         }
     }
     return line;
+}
+
+let isOpened = false;
+function isOpenFile() {
+    const open = document.getElementById('open');
+    const close = document.getElementById('close');
+    if (open && close) {
+        open.style.display = isOpened ? 'flex' : 'none';
+        close.style.display = !isOpened ? 'flex' : 'none';
+        isOpened = !isOpened;
+    }
+}
+
+function closeFile() {
+    const fields: string[] = ['method', 'import','class', 'enum','interface', 'attribute'];
+    fields.forEach(e => {
+        const field = document.getElementById(e);
+        if (field) {
+            field.innerHTML = '';
+            field.style.display = 'none';
+        }
+    })
+    const data = document.getElementById('output');
+    if (data) { data.innerHTML = ''}
+    isOpenFile();
 }
